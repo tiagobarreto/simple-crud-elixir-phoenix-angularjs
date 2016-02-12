aef56cc0a30bc08a1da39121aaab463ddd72e599
Tiago Barreto <falecom@tiagobarreto.com>
2016-02-12 19:49:21 -0200

    Remove unused references and fixed assets in brunch

--- brunch-config.js
+++ brunch-config.js
@@ -2,23 +2,13 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
-      // joinTo: {
-      //  "js/app.js": /^(web\/static\/js)/,
-      //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // https://github.com/brunch/brunch/tree/master/docs#concatenation
-      // order: {
-      //   before: [
-      //     "web/static/vendor/js/jquery-2.1.1.js",
-      //     "web/static/vendor/js/bootstrap.min.js"
-      //   ]
-      // }
+      joinTo: "js/app.js",
+      order: {
+        before: [
+          "web/static/vendor/js/angular.min.js",
+          "web/static/vendor/js/toastr.min.js"
+        ]
+      }
     },
     stylesheets: {
       joinTo: "css/app.css"
@@ -29,28 +19,20 @@ exports.config = {
   },
 
   conventions: {
-    // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/web/static/assets". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
     assets: /^(web\/static\/assets)/
   },
 
-  // Phoenix paths configuration
   paths: {
-    // Dependencies and current project directories to watch
     watched: [
       "web/static",
       "test/static"
     ],
 
-    // Where to compile files to
     public: "priv/static"
   },
 
-  // Configure your plugins
   plugins: {
     babel: {
-      // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
     }
   },
@@ -63,8 +45,6 @@ exports.config = {
 
   npm: {
     enabled: true,
-    // Whitelist the npm deps to be pulled in as front-end assets.
-    // All other deps in package.json will be excluded from the bundle.
     whitelist: ["phoenix", "phoenix_html"]
   }
 };
